61b475ffb4b2d96c4107650665252fdc623b0f69
satanas <wil.alejandro@gmail.com>
2015-03-21 17:54:18 -0300

    Fixed font

--- boot.js
+++ boot.js
@@ -3,7 +3,7 @@
 var bootState = {
   preload: function() {
     game.load.image('progressbar', 'assets/images/progressbar.png');
-    game.load.bitmapFont('open_sans', 'assets/fonts/open-sans.png', 'assets/fonts/open-sans.fnt');
+    game.load.bitmapFont('open_sans', 'assets/fonts/open_sans.png', 'assets/fonts/open_sans.fnt');
   },
 
   create: function() {
