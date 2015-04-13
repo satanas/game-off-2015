34967c95ac72bebbc4f96a4e8b05c7f8d5e4dae0
satanas <wil.alejandro@gmail.com>
2015-04-12 20:23:46 -0430

    Added new font

--- states/load.js
+++ states/load.js
@@ -13,6 +13,7 @@ var loadState = {
     game.load.bitmapFont('super', 'assets/fonts/press-start-super.png', 'assets/fonts/press-start-super.fnt');
     game.load.bitmapFont('ultra', 'assets/fonts/press-start-ultra.png', 'assets/fonts/press-start-ultra.fnt');
     game.load.bitmapFont('score', 'assets/fonts/press-start-score.png', 'assets/fonts/press-start-score.fnt');
+    game.load.bitmapFont('title', 'assets/fonts/press-start-title.png', 'assets/fonts/press-start-title.fnt');
 
     // Sprites
     game.load.image('title', 'assets/images/title.png');
