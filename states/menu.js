bab095c8ce48ef72dfe914d5af6e63bc6e7f239e
satanas <wil.alejandro@gmail.com>
2015-04-15 00:44:06 -0300

    Added credits

--- states/menu.js
+++ states/menu.js
@@ -4,7 +4,8 @@ var menuState = {
   create: function() {
     game.sound.stopAll();
     game.add.image(0, 0, 'title');
-    bitmapTextCentered(game.height - 30, 'instructions', 'Press ENTER to start', 12);
+    bitmapTextCentered(game.height - 70, 'instructions', 'Press ENTER to start', 12);
+    bitmapTextCentered(game.height - 20, 'instructions', 'Created by Wil Alvarez - @satanas82', 10);
 
     var enterKey = game.input.keyboard.addKey(Phaser.Keyboard.ENTER);
     enterKey.onDown.addOnce(this.start, this);
