6059d706a06b9b90ae6c80c9e2250ddc5c6829fe
satanas <wil.alejandro@gmail.com>
2015-04-16 00:57:11 -0300

    Included scene with instructions and controls

--- game.js
+++ game.js
@@ -23,6 +23,7 @@ var groups = {};
 game.state.add('boot', bootState);
 game.state.add('load', loadState);
 game.state.add('menu', menuState);
+game.state.add('instructions', instructionsState);
 game.state.add('play', playState);
 
 game.state.start('boot');
---
+++ states/instructions.js
@@ -0,0 +1,42 @@
+'use strict';
+
+var instructionsState = {
+  create: function() {
+    game.sound.stopAll();
+    game.add.image(0, 0, 'background');
+    var bg = this.game.add.graphics(0, 0);
+    bg.beginFill(0x000000, 1);
+    bg.drawRect(game.camera.x, game.camera.y, game.width, game.height);
+    bg.alpha = 0.5;
+    bg.endFill();
+    game.add.bitmapText(12, 60, 'instructions', 'Instructions', 20);
+    game.add.bitmapText(15, 90, 'instructions', '1. Take the blocks of code that', 12);
+    game.add.bitmapText(15, 105, 'instructions', '   fall from above', 12);
+    game.add.bitmapText(15, 135, 'instructions', '2. Combine the blocks in lines', 12);
+    game.add.bitmapText(15, 150, 'instructions', '   to create popular apps and', 12);
+    game.add.bitmapText(15, 165, 'instructions', '   deploy them', 12);
+    game.add.bitmapText(15, 195, 'instructions', '3. Missed blocks will become', 12);
+    game.add.bitmapText(15, 210, 'instructions', '   bugs', 12);
+    game.add.bitmapText(15, 240, 'instructions', '4. Unknown or buggy apps won\'t', 12);
+    game.add.bitmapText(15, 255, 'instructions', '   be deployed', 12);
+    game.add.bitmapText(15, 285, 'instructions', '5. If Octocat touch the top of', 12);
+    game.add.bitmapText(15, 300, 'instructions', '   the screen, you\'re fired', 12);
+    game.add.bitmapText(15, 330, 'instructions', '6. If an app has more than 4', 12);
+    game.add.bitmapText(15, 345, 'instructions', '   bugs, you\'re fired', 12);
+    game.add.bitmapText(12, 400, 'instructions', 'Controls', 20);
+    game.add.bitmapText(15, 430, 'instructions', '* Arrows to move and drop', 12);
+    game.add.bitmapText(15, 460, 'instructions', '* P to pause the game', 12);
+    game.add.bitmapText(15, 490, 'instructions', '* M to mute the music', 12);
+    //game.add.bitmapText(10, 125, 'instructions', '   the line is completed', 12);
+    //game.add.bitmapText(10, 155, 'instructions', '4. Each deploy gives you points', 12);
+    //game.add.bitmapText(10, 170, 'instructions', '   and the line will dissapear', 12);
+    bitmapTextCentered(game.height - 40, 'instructions', 'Press ENTER to continue', 12);
+
+    var enterKey = game.input.keyboard.addKey(Phaser.Keyboard.ENTER);
+    enterKey.onDown.addOnce(this.start, this);
+  },
+
+  start: function() {
+    game.state.start('play');
+  }
+};
